e9e037426cb7b4ad6604573feab9b9f7a5bb724a
AllWatcherG <92951041+AllWatcherG@users.noreply.github.com>
2023-03-05 18:27:49 -0500

    Complete

--- server.js
+++ server.js
@@ -7,7 +7,17 @@ const port = 3000;
 
 //TODO
 
+app.get("/musicians", async(request, response) =>{
+    try {
+        const musicians = await Musician.findAll(); // Retrieve all restaurants from the database
+        response.json(musicians); // Send the restaurants as a JSON response
+      } catch (error) {
+        console.error(error);
+        response.status(500).send('Internal Server Error');
+      }
+})
+
 app.listen(port, () => {
     sequelize.sync();
-    console.log(`Listening on port ${port}`)
+    console.log(`Your server is listening on port http://localhost:${port}/musicians`);
 })
