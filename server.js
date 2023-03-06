c21405c40cefb86d045a420c58e72ed586230633
AllWatcherG <92951041+AllWatcherG@users.noreply.github.com>
2023-03-06 15:38:09 -0500

    CRUD requests through postman

--- server.js
+++ server.js
@@ -24,6 +24,34 @@ app.get('/musicians/:id', async(req, res) => {
 
 })
 
+app.use(express.json())
+app.use(express.urlencoded())
+
+app.post('/musicians', async(req, res) => {
+  objToCreate = req.body
+  await Musician.create(objToCreate)
+  res.json(await Musician.findAll())
+})
+
+app.put('/musicians/:id', async(req, res) => {
+  objKeyToReplace = parseInt(req.params.id)
+  objFound = await Musician.findByPk(objKeyToReplace)
+
+  objFound.update({
+    name: req.body.name,
+    instrument: req.body.instrument
+  })
+
+  res.json(objFound)
+})
+
+app.delete('/musicians/:id', async(req, res) => {
+  objKeyToDelete = parseInt(req.params.id)
+  objFound = await Musician.findByPk(objKeyToDelete)
+  await objFound.destroy()
+  res.json(await Musician.findAll()) 
+})
+
 app.listen(port, () => {
     sequelize.sync();
     console.log(`Your server is listening on port http://localhost:${port}/musicians`);
