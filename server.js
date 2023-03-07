8fb3b6a8daa46d188943ad93c7cabe5312dadf45
AllWatcherG <92951041+AllWatcherG@users.noreply.github.com>
2023-03-07 13:17:28 -0500

    Router + Express Validator added

--- server.js
+++ server.js
@@ -2,55 +2,58 @@ const express = require("express");
 const app = express();
 const {Musician} = require("./Musician")
 const {sequelize} = require("./db")
-
+const musicianRouter = require("./routes/musicians")
 const port = 3000;
 
 //TODO
 
-app.get("/musicians", async(request, response) =>{
-    try {
-        const musicians = await Musician.findAll(); // Retrieve all restaurants from the database
-        response.json(musicians); // Send the restaurants as a JSON response
-      } catch (error) {
-        console.error(error);
-        response.status(500).send('Internal Server Error');
-      }
-})
+// app.get("/musicians", async(request, response) =>{
+//     try {
+//         const musicians = await Musician.findAll(); // Retrieve all restaurants from the database
+//         response.json(musicians); // Send the restaurants as a JSON response
+//       } catch (error) {
+//         console.error(error);
+//         response.status(500).send('Internal Server Error');
+//       }
+// })
 
-app.get('/musicians/:id', async(req, res) => {
-  keyForRes = req.params.id
-  objFound = await Musician.findByPk(keyForRes)
-  res.json(objFound)
+// app.get('/musicians/:id', async(req, res) => {
+//   keyForRes = req.params.id
+//   objFound = await Musician.findByPk(keyForRes)
+//   res.json(objFound)
 
-})
+// })
 
+
+
+// app.post('/musicians', async(req, res) => {
+//   objToCreate = req.body
+//   await Musician.create(objToCreate)
+//   res.json(await Musician.findAll())
+// })
+
+// app.put('/musicians/:id', async(req, res) => {
+//   objKeyToReplace = parseInt(req.params.id)
+//   objFound = await Musician.findByPk(objKeyToReplace)
+
+//   objFound.update({
+//     name: req.body.name,
+//     instrument: req.body.instrument
+//   })
+
+//   res.json(objFound)
+// })
+
+// app.delete('/musicians/:id', async(req, res) => {
+//   objKeyToDelete = parseInt(req.params.id)
+//   objFound = await Musician.findByPk(objKeyToDelete)
+//   await objFound.destroy()
+//   res.json(await Musician.findAll()) 
+// })
 app.use(express.json())
 app.use(express.urlencoded())
 
-app.post('/musicians', async(req, res) => {
-  objToCreate = req.body
-  await Musician.create(objToCreate)
-  res.json(await Musician.findAll())
-})
-
-app.put('/musicians/:id', async(req, res) => {
-  objKeyToReplace = parseInt(req.params.id)
-  objFound = await Musician.findByPk(objKeyToReplace)
-
-  objFound.update({
-    name: req.body.name,
-    instrument: req.body.instrument
-  })
-
-  res.json(objFound)
-})
-
-app.delete('/musicians/:id', async(req, res) => {
-  objKeyToDelete = parseInt(req.params.id)
-  objFound = await Musician.findByPk(objKeyToDelete)
-  await objFound.destroy()
-  res.json(await Musician.findAll()) 
-})
+app.use('/musicians', musicianRouter)
 
 app.listen(port, () => {
     sequelize.sync();
